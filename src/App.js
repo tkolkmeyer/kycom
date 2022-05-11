048a8981e1d5edf70ecbf2844ecfc923bd9e405b
Alien <kendgibsonjr@gmail.com>
2022-05-11 02:11:10 -0500

    css updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainHome'>
+
+      <div className = 'section1'>
+        <div className = "row">
+          <div className = "col-3-of-4">
+            col 3 of 4
+          </div>
+          <div className = "col-1-of-4">
+            col 1 of 4
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className= "section2">
+        this is section2
+        
+        <div className = "video">
+
+        </div>
+      
+      
+
+        <div className='footer'>
+          this is the footer 
+
+        </div>
+      </div>
     </div>
+    
+
+
+ 
   );
 }
 
