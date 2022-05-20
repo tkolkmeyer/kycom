df278c7e10cd55bcee20ca97e6a8b2dddd35747c
cyperpunkmunk <kendgibsonjr@gmail.com>
2022-05-20 12:17:46 -0500

    updaate

--- src/App.js
+++ src/App.js
@@ -8,31 +8,27 @@ import NftPage from './NFT/NFT.js';
 import Wav from './Wav/Wav.js';
 import Etc from './Etc/etc';
 
-
-
-
 const App  = () => {
 
-  function hi(){
-    
-  }
   
+  
+
   return(
     <>
 
       {/** mobile nav */}
           
-      <input class="menu-btn" id="active" type="checkbox"></input>
+      <input class="menu-btn" id="active" type="checkbox"  ></input>
           
           
-          <div class="wrapper">
-            <ul class = "menu">
-              <NavLink to="/" end> <li class="menuItem"> Home </li> </NavLink>
-              <NavLink to="nftPage" end> <li class="menuItem"> NFT </li> </NavLink>
-              <NavLink to="wav" end> <li class="menuItem"> .Wav </li> </NavLink>
-              <NavLink to="etc" end> <li class="menuItem"> Etc </li> </NavLink>
-            </ul>
-          </div>
+        <div class="wrapper">
+          <ul class = "menu">
+            <NavLink to="/" end> <li class="menuItem"> Home </li> </NavLink>
+            <NavLink to="nftPage" end> <li class="menuItem"> NFT </li> </NavLink>
+            <NavLink to="wav" end> <li class="menuItem"> .Wav </li> </NavLink>
+            <NavLink to="etc" end> <li class="menuItem"> Etc </li> </NavLink>
+          </ul>
+        </div>
     
 
       <SlideRoutes>
@@ -40,7 +36,6 @@ const App  = () => {
         <Route path="nftPage" element={<NftPage />} />
         <Route path="wav" element={<Wav />} />
         <Route path="etc" element={<Etc />} />
-        
       </SlideRoutes>
     
     </>
