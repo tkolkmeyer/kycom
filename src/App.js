7504d5f80e0a8415abf72c28bfeda78790697f71
Mason Pool <79488765+mjpool99@users.noreply.github.com>
2022-05-20 16:42:55 -0400

    Update to Mobile Navigation
    I wrote small amount of script to close the mobile navigation menu when menu item is clicked.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import './App.css';
 import React from 'react';
 import SlideRoutes from 'react-slide-routes';
 import { Route, NavLink } from 'react-router-dom';
-import Home from "./Home/home.js";
+import Home from "./Home/Home.js";
 import NftPage from './NFT/NFT.js';
 import Wav from './Wav/Wav.js';
 import Etc from './Etc/etc';
 
+
 const App  = () => {
-
   
   
-
+  const closeNav = () => {
+    let cb = document.getElementById("active");
+    if(cb.checked){
+      cb.checked = false;
+    }
+  }
+  
+  
   return(
     <>
 
       {/** mobile nav */}
           
-      <input class="menu-btn" id="active" type="checkbox"  ></input>
+      <input className="menu-btn" id="active" type="checkbox"  ></input>
           
           
-        <div class="wrapper">
-          <ul class = "menu">
-            <NavLink to="/" end> <li class="menuItem"> Home </li> </NavLink>
-            <NavLink to="nftPage" end> <li class="menuItem"> NFT </li> </NavLink>
-            <NavLink to="wav" end> <li class="menuItem"> .Wav </li> </NavLink>
-            <NavLink to="etc" end> <li class="menuItem"> Etc </li> </NavLink>
+        <div className="wrapper">
+          <ul className = "menu">
+            <NavLink onClick={closeNav} to="/" end> <li className="menuItem"> Home </li> </NavLink>
+            <NavLink onClick={closeNav} to="nftPage" end> <li className="menuItem"> NFT </li> </NavLink>
+            <NavLink onClick={closeNav} to="wav" end> <li className="menuItem"> .Wav </li> </NavLink>
+            <NavLink onClick={closeNav} to="etc" end> <li className="menuItem"> Etc </li> </NavLink>
           </ul>
         </div>
     
@@ -43,5 +50,7 @@ const App  = () => {
 };
 
 export default App;
+
+
 
 
