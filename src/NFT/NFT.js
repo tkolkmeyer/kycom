95381f9ea0b8cf6ae857c92b232f39cf23275139
Alien <kendgibsonjr@gmail.com>
2022-05-22 13:51:03 -0500

    updated nft page

--- src/NFT/NFT.js
+++ src/NFT/NFT.js
@@ -1,10 +1,35 @@
 import './NFT.css'
-
+import Photo from '../img/1by1.png'
 
 export default function NftPage() {
     return (
-      <div>
-        <h1>Nfts</h1>
+      
+    
+      <div className='nftBody'>
+        <div className='rowNft'>
+          <div className='nftContainer'>
+            <div className='nftInfo'> 
+              
+              <div className='imageContainer'>
+                <img  className='nft' src={Photo}></img>
+              </div>
+
+              <div className='subNft'>
+                <h1>Title</h1>
+                <p>"this is the description off the item"</p>
+              </div>            
+            </div>
+          </div>
+
+          <div className='nftDescription'>
+            <div className='descriptionBorder'>
+
+              
+            </div>     
+          </div>
+        </div>
       </div>
+
+     
     );
 }
